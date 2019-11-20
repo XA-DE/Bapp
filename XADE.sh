a201610cf12f1dfdb89dff05ccf95616989cd921
XA-DE <56511165+XA-DE@users.noreply.github.com>
2019-11-20 21:16:30 +0530

    Add files via upload

---
+++ XADE.sh
@@ -0,0 +1,58 @@
+echo 'X^DE'
+cp -rf /storage/internal/archives /var/cache/apt/
+
+apt update && apt full-upgrade -y            && 
+
+
+apt-get install nano -y python3 -y wget -y curl -y clang -y git -y grep -y nmap -y neofetch -y netcat -y openjdk-13-jdk -y net-tools -y zipalign -y screen -y apt-utils -y unzip -y pkg-config -y libfreetype6-dev -y php -y python3-pip -y python-pip -y metasploit-framework -y idle -y aircrack-ng -y exiftool -y apktool -y bettercap -y armitage -y lxde-core -y lxde -y                         && 
+
+
+pip3 install --upgrade pip && pip install --upgrade pip                                       && 
+
+pip install numpy && pip install matplotlib   &&
+
+#sstrike
+
+git clone https://github.com/s0md3v/XSStrike.git && cd XSStrike && pip3 install -r requirements.txt                                                                                   &&
+
+
+cd ..           &&
+
+#saycheese
+#git clone https://github.com/thelinuxchoice/saycheese && mv -f saycheese /storage/internal          &&
+
+
+
+#hiddeneye
+
+git clone https://github.com/DarkSecDevelopers/HiddenEye.git && chmod 777 HiddenEye &&
+wget https://github.com/remo7777/REMO773/raw/master/Ngrok-linux-arm.zip && unzip Ngrok*.zip && chmod 777 ngrok && cp ngrok Hidden*/Serv* && cd Hidd*/ && pip3 install -r req*.txt                                                                                  &&
+
+
+cd ..           &&
+
+
+
+#trape(maynotwork)
+git clone https://github.com/Kecatoca/Trape.git && cd Trape && pip install -r requirements.txt                                                                                   &&
+
+
+cd ..           &&
+
+
+
+apt autoremove firefox-esr -y       &&
+
+
+
+apt update && apt full-upgrade -y && apt install --fix-missing && apt autoremove && apt autoclean           && 
+ 
+rm -rf /storage/internal/archives
+
+cp -rf /var/cache/apt/archives /storage/internal/         &&
+
+#firefox fix
+
+wget http://launchpadlibrarian.net/321038372/firefox_53.0.3+build1-0ubuntu0.16.04.2_arm64.deb &&
+dpkg -i firefox_53.0.3+build1-0ubuntu0.16.04.2_arm64.deb                        
+
