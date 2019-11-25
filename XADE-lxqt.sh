f55258f33e08180c7de0f71d1b76a457d0716248
XA-DE <56511165+XA-DE@users.noreply.github.com>
2019-11-25 12:11:45 +0000

    Delete XADE-lxqt.sh

--- XADE-lxqt.sh
+++
@@ -1,6 +0,0 @@
-wget http://launchpadlibrarian.net/321038372/firefox_53.0.3+build1-0ubuntu0.16.04.2_arm64.deb &&
-#LXQT
-wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/LXQt/de-apt-lxqt.sh && bash de-apt-lxqt.sh
-apt purge firefox-esr -y
-dpkg -i firefox_53.0.3+build1-0ubuntu0.16.04.2_arm64.deb
-apt install -f -y
