bed7c8c87cc064eae297202009ee5040592eb648
XA-DE <56511165+XA-DE@users.noreply.github.com>
2019-11-25 08:08:50 +0000

    Update XADE.sh

--- XADE.sh
+++ XADE.sh
@@ -5,7 +5,6 @@ apt update -y
 apt full-upgrade -y
 
 apt install python3-pip python-pip wget curl clang git grep netcat net-tools screen apt-utils unzip pkg-config libfreetype6-dev php metasploit-framework -y &&
-apt install lxqt-core -y &&
 # apt install virtualenv bettercap armitage aircrack-ng nmap sparta apktool openjdk-13-jdk idle zipalign neofetch exiftool -y &&
 pip3 install --upgrade pip &&
 #pip2 install --upgrade pip
@@ -14,8 +13,7 @@ pip3 install matplotlib &&
 
 #sstrike
 git clone https://github.com/s0md3v/XSStrike.git && cd XSStrike &&
-pip3 install -r requirements.txt
-pip install -r requirements.txt
+#pip3 install -r req*.txt
 cd .. &&   
 
 #shellphish
@@ -24,20 +22,14 @@ cd .. &&
 
 #trape(maynotwork)
 git clone https://github.com/Kecatoca/Trape.git && cd Trape && pip2 install -r requirements.txt &&
-pip2 uninstall six -y
-pip2 install six
+#pip2 install -r req*.txt
+
+
 cd .. &&
 
-apt purge udisks2 firefox-esr -y
+apt purge udisks2
 apt update
 apt full-upgrade -y
 apt install -f -y
 apt autoremove -y
-apt autoclean &&
-
-#firefox fix
-
-wget http://launchpadlibrarian.net/321038372/firefox_53.0.3+build1-0ubuntu0.16.04.2_arm64.deb &&
-dpkg -i firefox_53.0.3+build1-0ubuntu0.16.04.2_arm64.deb
-
-apt install -f -y
+apt autoclean
