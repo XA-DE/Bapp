8ba46b28698832eb58902a75bf714f6554559da3
XA-DE <56511165+XA-DE@users.noreply.github.com>
2019-11-25 11:47:11 +0000

    Update XADE-lxqt.sh

--- XADE-lxqt.sh
+++ XADE-lxqt.sh
@@ -1,9 +1,6 @@
-echo -e "\x1b[1m\x1b[36m 『『Xa͜͡dE』』"
-#LXQT
-wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/LXQt/de-apt-lxqt.sh && bash de-apt-lxqt.sh &&
-vncserver-stop &&
-#firefox fix
 wget http://launchpadlibrarian.net/321038372/firefox_53.0.3+build1-0ubuntu0.16.04.2_arm64.deb &&
+#LXQT
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/LXQt/de-apt-lxqt.sh && bash de-apt-lxqt.sh
 apt purge firefox-esr -y
 dpkg -i firefox_53.0.3+build1-0ubuntu0.16.04.2_arm64.deb
 apt install -f -y
